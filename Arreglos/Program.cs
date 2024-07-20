9a09d348425d8cc50c34fa1f370b274b4ce55137
Cristo Vazquez <c.j_13_10@hotmail.com>
2024-07-20 14:26:36 -0700

    Probando funcionalidad

--- Arreglos/Program.cs
+++ Arreglos/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Pilas pilas = new Pilas(3);  
+
+            try
+            {
+                Pilas pilas = new Pilas(4);
+                pilas.Agregar("A");
+                Console.WriteLine(pilas.ObtenerDatos());
+                pilas.Agregar("B");
+                Console.WriteLine(pilas.ObtenerDatos());
+                pilas.Agregar("C");
+                Console.WriteLine(pilas.ObtenerDatos());
+                pilas.Agregar("D");
+                Console.WriteLine(pilas.ObtenerDatos());
+                pilas.Agregar("E");
+                Console.WriteLine("Eliminando elementos");
+                pilas.Eliminar();
+                Console.WriteLine(pilas.ObtenerDatos());
+                pilas.Eliminar();
+                Console.WriteLine(pilas.ObtenerDatos());
+                pilas.Eliminar();
+                Console.WriteLine(pilas.ObtenerDatos());
+                pilas.Eliminar();
+                Console.WriteLine(pilas.ObtenerDatos());
+                pilas.Eliminar();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
